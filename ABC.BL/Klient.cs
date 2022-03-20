9b009101a0be461329c6d72ba1ce441112572747
MarcinPierchala <macintecmarcin@gmail.com>
2022-03-20 20:26:23 +0100

    Dodaj pliki projektów.

---
+++ ABC.BL/Klient.cs
@@ -0,0 +1,57 @@
+﻿namespace ABC.BL
+{
+    public class Klient
+    {
+        public static int Counter { get; set; }
+
+        private string _nazwisko;
+        
+        public string Nazwisko
+        {
+            get 
+            { 
+                return _nazwisko; 
+            }
+            set 
+            { 
+                //if (string.IsNullOrWhiteSpace(value))
+                    _nazwisko = value; 
+            }
+        }
+
+        public string Imie { get; set; }
+
+        public string Email { get; set; }
+
+        public int KlientId { get; private set; }
+
+        public string ImieNazwisko 
+        { 
+            get 
+            {
+                string imieNazwisko = Imie;
+                if (!string.IsNullOrWhiteSpace(Nazwisko))
+                {
+                    if (!string.IsNullOrWhiteSpace(imieNazwisko))
+                    {
+                        imieNazwisko += ", ";
+                    }
+                    imieNazwisko += Nazwisko;
+                }
+                return imieNazwisko; 
+            } 
+        }
+
+        public bool Zwaliduj()
+        {
+            var poprawne = true;
+            if (string.IsNullOrWhiteSpace(Nazwisko))
+                poprawne = false;
+            if (string.IsNullOrWhiteSpace(Email))
+                poprawne |= false;
+
+            return poprawne;
+        }
+
+    }
+}
---
+++ ABC.BLTest/KlientTest.cs
@@ -0,0 +1,117 @@
+using ABC.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class KlientTest
+    {
+        [TestMethod]
+        public void ImieNazwiskoTest()
+        {
+            //Arrange
+            Klient klient = new Klient();
+            klient.Imie = "Marcin";
+            klient.Nazwisko = "Pierchala";
+            string wartoscOczekiwana = "Marcin, Pierchala";
+            //ACT
+            string aktualnaWartosc = klient.ImieNazwisko;
+            //Assert
+            Assert.AreEqual(wartoscOczekiwana, aktualnaWartosc);
+        }
+
+        [TestMethod]
+        public void ImieNazwiskoImiePuste()
+        {
+            //Arrange
+            Klient klient = new Klient();
+            klient.Nazwisko = "Pierchala";
+            string wartoscOczekiwana = "Pierchala";
+            //ACT
+            string aktualnaWartosc = klient.ImieNazwisko;
+            //Assert
+            Assert.AreEqual(wartoscOczekiwana, aktualnaWartosc);
+        }
+
+        [TestMethod]
+        public void ImieNazwiskoNazwiskoPuste()
+        {
+            //Arrange
+            Klient klient = new Klient();
+            klient.Imie = "Marcin";
+            string wartoscOczekiwana = "Marcin";
+            //ACT
+            string aktualnaWartosc = klient.ImieNazwisko;
+            //Assert
+            Assert.AreEqual(wartoscOczekiwana, aktualnaWartosc);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            //Arrange
+            Klient klient1 = new Klient();
+            klient1.Imie = "Marcin";
+            Klient.Counter += 1;
+
+            Klient klient2 = new Klient();
+            klient2.Imie = "Rafa³";
+            Klient.Counter += 1;
+
+            Klient klient3 = new Klient();
+            klient3.Imie = "Adian";
+            Klient.Counter += 1;
+
+            //ACT
+
+            //Assert
+            Assert.AreEqual(3, Klient.Counter);
+        }
+
+        [TestMethod]
+        public void ZwalidujTest()
+        {
+            //Arrange
+            var klient = new Klient();
+            klient.Nazwisko = "Pierchla";
+            klient.Email = "macintecmarcin@gmail.com";
+            var wartoscOczekiwana = true;
+
+            //ACT
+            var aktualnaWartosc = klient.Zwaliduj();
+
+            //Assert
+            Assert.AreEqual(wartoscOczekiwana, aktualnaWartosc);
+        }
+
+        [TestMethod]
+        public void ZwalidujNazwiskoPuste()
+        {
+            //Arrange
+            var klient = new Klient();
+            klient.Email = "macintecmarcin@gmail.com";
+            var wartoscOczekiwana = false;
+
+            //ACT
+            var aktualnaWartosc = klient.Zwaliduj();
+
+            //Assert
+            Assert.AreEqual(wartoscOczekiwana, aktualnaWartosc);
+        }
+
+       /* [TestMethod]
+        public void ZwalidujEmailPusty()
+        {
+            //Arrange
+            var klient = new Klient();
+            klient.Nazwisko = "Pierchla";
+            var wartoscOczekiwana = false;
+
+            //ACT
+            var aktualnaWartosc = klient.Zwaliduj();
+
+            //Assert
+            Assert.AreEqual(wartoscOczekiwana, aktualnaWartosc);
+        }*/
+    }
+}
