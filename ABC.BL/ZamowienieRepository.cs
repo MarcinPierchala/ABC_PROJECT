005e193419bb169e24ca193e66c76ce01086a57d
MarcinPierchala <macintecmarcin@gmail.com>
2022-03-22 19:08:26 +0100

    After_5_part_of_the_Learning

---
+++ ABC.BL/ZamowienieRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.BL
+{
+    public class ZamowienieRepository
+    {
+        /// <summary>
+        /// Pobieranie jedno zamowienie
+        /// </summary>
+        /// <param name="zamowienieId"></param>
+        /// <returns></returns>
+        public Zamowienie Pobierz(int zamowienieId)
+        {
+            Zamowienie zamowienie = new Zamowienie(zamowienieId);
+
+            if(zamowienieId == 10)
+            {
+                zamowienie.DataZamowienia = new DateTimeOffset(2022, 3, 22, 22, 22, 22, new TimeSpan(7, 0, 0));
+            }
+            return zamowienie;
+        }
+
+        /// <summary>
+        /// Zapisanie aktualnego zamowienia
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            return true;
+        }
+    }
+}
---
+++ ABC.BLTest/zamowienieRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ABC.BL;
+using System;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class ZamowienieRepositoryTest
+    {
+        [TestMethod]
+
+        public void PobierzZamowienie()
+        {
+            var zamowienieRepository = new ZamowienieRepository();
+
+            var oczekiwanaWartosc = new Zamowienie(10)
+            {
+                DataZamowienia = new DateTimeOffset(2022, 3, 22, 22, 22, 22, new TimeSpan(7, 0, 0))
+            };
+
+            var aktualnaWartosc = zamowienieRepository.Pobierz(10);
+
+            Assert.AreEqual(oczekiwanaWartosc.ZamowienieId, aktualnaWartosc.ZamowienieId);
+            Assert.AreEqual(oczekiwanaWartosc.DataZamowienia, aktualnaWartosc.DataZamowienia);
+        }
+    }
+}
